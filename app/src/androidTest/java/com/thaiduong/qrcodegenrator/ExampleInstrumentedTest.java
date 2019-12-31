4c1d38beaa8f595f9f288b474ea9f764e1fc569c
Thai Duong Vu <vu.t.duong01@gmail.com>
2019-12-31 08:16:03 -0500

    Configure for IntelliJ

--- app/src/androidTest/java/com/thaiduong/qrcodegenrator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/thaiduong/qrcodegenrator/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.thaiduong.qrcodegenrator", appContext.getPackageName());
+        assertEquals("com.thaiduong.qrcodegenerator", appContext.getPackageName());
     }
 }
