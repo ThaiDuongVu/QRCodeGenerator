7b3d57322fb172e3c17fb11b3b7f5c9e9c30d5ee
Thai Duong Vu <vu.t.duong01@gmail.com>
2020-01-02 22:12:01 -0500

    Save image to storage implemented

--- app/src/main/java/com/thaiduong/qrcodegenrator/MainActivity.java
+++ app/src/main/java/com/thaiduong/qrcodegenrator/MainActivity.java
@@ -3,6 +3,10 @@ package com.thaiduong.qrcodegenrator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,6 +14,9 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +40,29 @@ public class MainActivity extends AppCompatActivity {
         else {
             final String url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + generateEditText.getText().toString();
             Picasso.get().load(url).into(QRImageView);
+        }
+    }
+
+    public void onSaveButtonPressed(View view){
+        BitmapDrawable drawable = (BitmapDrawable) QRImageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        File file = new File(directory, "UniqueFileName" + ".jpg");
+
+        FileOutputStream fos;
+
+        try {
+            fos = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            Toast.makeText(this, "Image saved!", Toast.LENGTH_SHORT).show();
+
+            fos.flush();
+            fos.close();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Exception!", Toast.LENGTH_SHORT).show();
         }
     }
 }
